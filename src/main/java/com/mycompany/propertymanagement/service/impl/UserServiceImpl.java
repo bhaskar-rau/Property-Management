47cfffea7f65a58cda9c6780e6822bce6db05077
Bhaskar <bhaskar@ensarsolutions.com>
2023-12-03 00:24:27 +0530

    ErrorModel and Business Exception

--- src/main/java/com/mycompany/propertymanagement/service/impl/UserServiceImpl.java
+++ src/main/java/com/mycompany/propertymanagement/service/impl/UserServiceImpl.java
@@ -4,10 +4,17 @@ package com.mycompany.propertymanagement.service.impl;
 import com.mycompany.propertymanagement.converter.UserConverter;
 import com.mycompany.propertymanagement.dto.UserDTO;
 import com.mycompany.propertymanagement.entity.UserEntity;
+import com.mycompany.propertymanagement.exception.BusinessException;
+import com.mycompany.propertymanagement.exception.ErrorModel;
 import com.mycompany.propertymanagement.repository.UserRepository;
 import com.mycompany.propertymanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,14 +24,39 @@ public class UserServiceImpl implements UserService {
     private UserConverter userConverter;
     @Override
     public UserDTO register(UserDTO userDTO) {
-        UserEntity userEntity =userConverter.convertDTOtoEntity(userDTO);
-        userEntity = userRepository.save(userEntity);
-        userDTO = userConverter.convertEntitytoDTO(userEntity);
-        return userDTO;
+        Optional<UserEntity> optUe = userRepository.findByOwnerEmail(userDTO.getOwnerEmail());
+        if(optUe.isPresent()){
+            List<ErrorModel> errorModelList = new ArrayList<>();
+            ErrorModel errorModel = new ErrorModel();
+            errorModel.setCode("EMAIL_ALREADY_EXISTS");
+            errorModel.setMessage("The email you are trying to register already exists");
+            errorModelList.add(errorModel);
+            throw new BusinessException(errorModelList);
+        }else {
+            UserEntity userEntity =userConverter.convertDTOtoEntity(userDTO);
+            userEntity = userRepository.save(userEntity);
+            userDTO = userConverter.convertEntitytoDTO(userEntity);
+            return userDTO;
+        }
+
     }
 
     @Override
     public UserDTO login(String email, String password) {
-        return null;
+        UserDTO userDTO = null;
+       Optional<UserEntity> optionalUserEntity = userRepository.findByOwnerEmailAndPassword(email, password);
+       if(optionalUserEntity.isPresent()){
+           userDTO = userConverter.convertEntitytoDTO(optionalUserEntity.get());
+       } else {
+           List<ErrorModel> errorModelList = new ArrayList<>();
+           ErrorModel errorModel = new ErrorModel();
+           errorModel.setCode("INVALID_LOGIN");
+           errorModel.setMessage("Incorrect email or password");
+           errorModelList.add(errorModel);
+
+           throw new BusinessException(errorModelList);
+       }
+        return userDTO;
+
     }
 }
