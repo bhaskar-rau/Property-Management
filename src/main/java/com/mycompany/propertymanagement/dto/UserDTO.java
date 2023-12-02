867eaa33c8444dfde40dde1c0a24b1fd2488c71b
Bhaskar <bhaskar@ensarsolutions.com>
2023-12-03 03:14:28 +0530

    Implemented logging

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.mycompany.propertymanagement.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +15,12 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "owner email is mandatory")
+    @NotEmpty(message = "email cannot be null")
+    @Size(min = 1, max = 50, message = "Owner Email should be between 1 to 50 characters in length")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "password cannot be null")
+    @NotEmpty(message = "password cannot be empty")
     private String password;
 }
